7e5da4f3c30cd9ae9783c04a30470d6c91cae113
masonwgomes <122919650+masonwgomes@users.noreply.github.com>
2024-04-21 12:11:01 -0400

    initial

---
+++ script.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var phoneIcon = document.getElementById("phoneIcon");
+    var phoneNumber = document.getElementById("phoneNumber");
+
+    // Show phone number when icon is clicked
+    phoneIcon.addEventListener("click", function() {
+        phoneNumber.style.display = "block";
+    });
+
+    // Hide phone number when mouse leaves the icon
+    phoneIcon.addEventListener("mouseleave", function() {
+        phoneNumber.style.display = "none";
+    });
+});
